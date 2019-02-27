15d412472ae15548e51e9ed00fe52fa7dccecf3c
Jian Bao <jian.bao@shopee.com>
2019-02-27 16:08:11 +0800

    update demo

--- src/demo.js
+++ src/demo.js
@@ -1,10 +1,9 @@
 function Example() {
     const [count, setCount] = React.useState(0)
-    const newValue = count + 1
     return (
         <div>
             <p>You clicked {count} times</p>
-            <button onClick={() => setCount(newValue)} >
+            <button onClick={() => setCount(count + 1)} >
                 Click me
             </button>
         </div>
